828337122a0b52e25e675307134181b9fcb3be03
Jatin200521 <jatinmeena782@gmail.com>
2024-06-17 12:53:44 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,59 +1,77 @@
-const BASE_URL =
-  "https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies";
+// include api for currency change
 
-const dropdowns = document.querySelectorAll(".dropdown select");
-const btn = document.querySelector("form button");
-const fromCurr = document.querySelector(".from select");
-const toCurr = document.querySelector(".to select");
-const msg = document.querySelector(".msg");
+const api = "https://api.exchangerate-api.com/v4/latest/USD";
 
-for (let select of dropdowns) {
-  for (currCode in countryList) {
-    let newOption = document.createElement("option");
-    newOption.innerText = currCode;
-    newOption.value = currCode;
-    if (select.name === "from" && currCode === "USD") {
-      newOption.selected = "selected";
-    } else if (select.name === "to" && currCode === "INR") {
-      newOption.selected = "selected";
-    }
-    select.append(newOption);
-  }
+// for selecting different controls
 
-  select.addEventListener("change", (evt) => {
-    updateFlag(evt.target);
-  });
+var search = document.querySelector(".searchBox");
+
+var convert = document.querySelector(".convert");
+
+var fromCurrecy = document.querySelector(".from");
+
+var toCurrecy = document.querySelector(".to");
+
+var finalValue = document.querySelector(".finalValue");
+
+var finalAmount = document.getElementById("finalAmount");
+
+var resultFrom;
+
+var resultTo;
+
+var searchValue;
+
+// Event when currency is changed
+
+fromCurrecy.addEventListener("change", (event) => {
+  resultFrom = `${event.target.value}`;
+});
+
+// Event when currency is changed
+
+toCurrecy.addEventListener("change", (event) => {
+  resultTo = `${event.target.value}`;
+});
+
+search.addEventListener("input", updateValue);
+
+// function for updating value
+
+function updateValue(e) {
+  searchValue = e.target.value;
 }
 
-const updateExchangeRate = async () => {
-  let amount = document.querySelector(".amount input");
-  let amtVal = amount.value;
-  if (amtVal === "" || amtVal < 1) {
-    amtVal = 1;
-    amount.value = "1";
-  }
-  const URL = `${BASE_URL}/${fromCurr.value.toLowerCase()}/${toCurr.value.toLowerCase()}.json`;
-  let response = await fetch(URL);
-  let data = await response.json();
-  let rate = data[toCurr.value.toLowerCase()];
+// when user clicks, it calls function getresults
 
-  let finalAmount = amtVal * rate;
-  msg.innerText = `${amtVal} ${fromCurr.value} = ${finalAmount} ${toCurr.value}`;
-};
+convert.addEventListener("click", getResults);
 
-const updateFlag = (element) => {
-  let currCode = element.value;
-  let countryCode = countryList[currCode];
-  let newSrc = `https://flagsapi.com/${countryCode}/flat/64.png`;
-  let img = element.parentElement.querySelector("img");
-  img.src = newSrc;
-};
+// function getresults
 
-btn.addEventListener("click", (evt) => {
-  evt.preventDefault();
-  updateExchangeRate();
-});
+function getResults() {
+  fetch(`${api}`)
+    .then((currency) => {
+      return currency.json();
+    })
+    .then(displayResults);
+}
 
-window.addEventListener("load", () => {
-  updateExchangeRate();
-});
+// display results after convertion
+
+function displayResults(currency) {
+  let fromRate = currency.rates[resultFrom];
+
+  let toRate = currency.rates[resultTo];
+
+  finalValue.innerHTML = ((toRate / fromRate) * searchValue).toFixed(2);
+
+  finalAmount.style.display = "block";
+}
+
+// when user click on reset button
+
+function clearVal() {
+  window.location.reload();
+
+  document.getElementsByClassName("finalValue").innerHTML = "";
+}
